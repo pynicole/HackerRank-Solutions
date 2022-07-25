0005a26260ab032a7630edc584f910b134f9f2d6
pynicole <nicolerodgers99@gmail.com>
2022-07-25 18:57:55 +0000

    Create p05_Japanese_Cities_Attributes.sql

---
+++ sql/p05_Japanese_Cities_Attributes.sql
@@ -0,0 +1,23 @@
+/*
+
+Query all attributes of every Japanese city in the CITY table. The COUNTRYCODE for Japan is JPN.
+
+The CITY table is described as follows:
+
+==============================
+| FIELD       | TYPE         |
+==============================
+| ID          | NUMBER       |
+------------------------------
+| NAME        | VARCHAR2(17) |
+------------------------------
+| COUNTRYCODE | VARCHAR2(3)  |
+------------------------------
+| DISTRICT    | VARCHAR2(20) |
+------------------------------
+| POPULATION  | NUMBER       |
+------------------------------
+
+*/
+
+SELECT * FROM City WHERE countrycode="JPN"
