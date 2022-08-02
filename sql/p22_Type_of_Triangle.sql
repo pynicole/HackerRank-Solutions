d2d29ae38e80301ab6313b20aabfbc5b10eaae2b
pynicole <nicolerodgers99@gmail.com>
2022-08-02 21:21:34 +0000

    Create p22_Type_of_Triangle.sql

---
+++ sql/p22_Type_of_Triangle.sql
@@ -0,0 +1,32 @@
+/*
+
+Write a query identifying the type of each record in the TRIANGLES table using its three side lengths. Output one of the following statements for each record in the table:
+
+Equilateral: It's a triangle with 3 sides of equal length.
+Isosceles: It's a triangle with 2 sides of equal length.
+Scalene: It's a triangle with 3 sides of differing lengths.
+Not A Triangle: The given values of A, B, and C don't form a triangle.
+
+The TRIANGLES table is described as follows:
+
+====================
+| COLUMN | TYPE    |
+====================
+| A      | INTEGER |
+--------------------
+| B      | INTEGER |
+--------------------
+| C      | INTEGER |
+--------------------
+
+*/
+
+
+SELECT 
+CASE
+    WHEN A=B AND B=C THEN 'Equilateral'
+    WHEN A=B AND A+B>C OR A=C AND A+B>C THEN 'Isosceles'
+    WHEN A!=B AND B!=C AND A+B>C THEN 'Scalene'
+    ELSE 'Not A Triangle'
+END AS Test
+FROM Triangles
