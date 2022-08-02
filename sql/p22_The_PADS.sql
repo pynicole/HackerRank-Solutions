709f35a1b66ca46186f59be28fe5d5b58b2ed202
pynicole <nicolerodgers99@gmail.com>
2022-08-02 21:26:06 +0000

    Update p22_The_PADS.sql

--- sql/p22_The_PADS.sql
+++ sql/p22_The_PADS.sql
@@ -9,12 +9,12 @@ Query the number of ocurrences of each occupation in OCCUPATIONS. Sort the occur
   
   There are a total of [occupation_count] [occupation]s.
  
- where [occupation_count] is the number of occurrences of an occupation in OCCUPATIONS and [occupation] is the lowercase occupation name. 
- If more than one Occupation has the same [occupation_count], they should be ordered alphabetically.
+where [occupation_count] is the number of occurrences of an occupation in OCCUPATIONS and [occupation] is the lowercase occupation name. 
+If more than one Occupation has the same [occupation_count], they should be ordered alphabetically.
   
- Note: There will be at least two entries in the table for each type of occupation.
+Note: There will be at least two entries in the table for each type of occupation.
  
- The OCCUPATIONS table is described as follows:
+The OCCUPATIONS table is described as follows:
  
 =======================
 | COLUMN     | TYPE   |
