f4470f06b36e318fd978d28fa0df3459992fa99f
pynicole <nicolerodgers99@gmail.com>
2022-08-03 16:01:44 +0000

    Create p26_Binary_Tree_Nodes.sql

---
+++ sql/p26_Binary_Tree_Nodes.sql
@@ -0,0 +1,28 @@
+/*
+
+You are given a table, BST, containing two columns: N and P, where N represents the value of a node in Binary Tree, and P is the parent of N.
+
+Write a query to find the node type of Binary Tree ordered by the value of the node. Output one of the following for each node:
+
+Root: If node is root node.
+Leaf: If node is leaf node.
+Inner: If node is neither root nor leaf node.
+
+====================
+| COLUMN | TYPE    |
+====================
+| N      | INTEGER |
+--------------------
+| P      | INTEGER |
+--------------------
+
+*/
+
+SELECT N,
+CASE
+   WHEN P IS NULL THEN 'Root'
+   WHEN N IN (SELECT DISTINCT P FROM BST) THEN 'Inner'
+   ELSE 'Leaf'
+END AS Temp
+FROM BST
+ORDER BY N ASC
