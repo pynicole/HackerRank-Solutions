7b2fe5394116265c0e5a3eb57885aa21471909db
pynicole <nicolerodgers99@gmail.com>
2022-08-09 15:07:03 +0000

    Create p29_The_Blunder.sql

---
+++ sql/p29_The_Blunder.sql
@@ -0,0 +1,22 @@
+/*
+
+Samantha was tasked with calculating the average monthly salaries for all employees in the EMPLOYEES table, but did not realize her keyboard's 0 key was broken until after completing the calculation. 
+She wants your help finding the difference between her miscalculation (using salaries with any zeros removed), and the actual average salary.
+
+Write a query calculating the amount of error (i.e.: actual - miscalculated average monthly salaries), and round it up to the next integer.
+
+The EMPLOYEES table is described as follows:
+
+====================
+| COLUMN | TYPE    |
+====================
+| ID     | INTEGER |
+--------------------
+| NAME   | STRING  |
+--------------------
+| SALARY | INTEGER |
+--------------------
+
+*/
+
+SELECT CEIL((AVG(salary)) - (AVG(REPLACE(salary, '0', '')))) FROM Employees;
