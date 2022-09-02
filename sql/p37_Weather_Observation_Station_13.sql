de264f97b70adca568097e07980cc09bf78392ee
pynicole <nicolerodgers99@gmail.com>
2022-09-02 13:26:16 -0400

    Create p37_Weather_Observation_Station_13.sql

---
+++ sql/p37_Weather_Observation_Station_13.sql
@@ -0,0 +1,25 @@
+/*
+
+Query the sum of Northern Latitudes (LAT_N) from STATION having values greater than 38.7880 and less than 137.2345. Truncate your answer to 4 decimal places.
+
+The STATION table is described as follows:
+   
+=========================
+| FIELD  | TYPE         |
+=========================
+| ID     | NUMBER       |
+-------------------------
+| CITY   | VARCHAR2(21) |
+-------------------------
+| STATE  | VARCHAR2(2)  |
+-------------------------
+| LAT_N  | NUMBER       |
+-------------------------
+| LONG_W | NUMBER       |
+-------------------------
+
+*/
+
+SELECT ROUND(SUM(Lat_N), 4)
+FROM Station
+WHERE Lat_N>38.7880 AND Lat_N<137.2345
